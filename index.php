24b2b4a2107feb59560dc84e6d999f515d398e0a
Brody14 <stefania.spinas@gmail.com>
2023-03-30 14:52:50 +0200

    FEAT class

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+class Movie {
+    public string $title;
+    public string $directorFirstName;
+    public string $directorLastName;
+    public int $year;
+    public string $genre;
+    public float $rating;
+}
+
+?>
