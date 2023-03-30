161b5962e7411f3bca48d0ae57225c662a3f74c4
Brody14 <stefania.spinas@gmail.com>
2023-03-30 15:13:40 +0200

    FEAT instances

--- index.php
+++ index.php
@@ -15,6 +15,26 @@ class Movie {
     function getDirectorFullName() {
         return $this->directorFirstName . " " . $this->directorLastName;
     }
+
 }
+
+$movie1 = new Movie("Space Jam");
+$movie1->directorFirstName = "Joe";
+$movie1->directorLastName = "Pytka";
+$movie1->year = 1996;
+$movie1->genre = "Animazione";
+$movie1->rating = 6.5;
+var_dump($movie1);
+var_dump($movie1->getDirectorFullName());
+
+$movie2 = new Movie("Space Jam 2 - New Legends");
+$movie2->directorFirstName = "Malcom D.";
+$movie2->directorLastName = "Lee";
+$movie2->year = 2021;
+$movie2->genre = "Animazione";
+$movie2->rating = 4.5;
+var_dump($movie2);
+var_dump($movie2->getDirectorFullName());
+
 
 ?>
